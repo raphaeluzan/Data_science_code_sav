d1c085b66f13bba7fb618e24300a4ac041a53669
raphaeluzan <raphael.uzan.aa@gmail.com>
2019-05-23 16:48:58 +0200

    Create README.py

---
+++ scikitlearn/randomforest/README.py
@@ -0,0 +1,10 @@
+Model 1 MAE: 24015
+Model 2 MAE: 23740
+Model 3 MAE: 23528
+Model 4 MAE: 23996
+Model 5 MAE: 23706
+
+
+
+https://medium.com/human-in-a-machine-world/mae-and-rmse-which-metric-is-better-e60ac3bde13d
+[text](https://medium.com/human-in-a-machine-world/mae-and-rmse-which-metric-is-better-e60ac3bde13d)

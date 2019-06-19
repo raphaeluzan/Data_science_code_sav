8d0eddf917a2f593348e183e1c2fad83ab1386ef
raphaeluzan <raphael.uzan.aa@gmail.com>
2019-06-19 18:30:10 +0200

    Update pysparkDiff.py

--- BIG_DATA/pysparkDiff.py
+++ BIG_DATA/pysparkDiff.py
@@ -1,18 +1,16 @@
-lines = sc.textFile("/Users/raphaeluzan/Desktop/mapreduce/data.txt")
-lines = lines.map(lambda x : (x.split('\t')[0],x.split('\t')[1].split()))
-lines.reduceByKey(lambda x,y: x+y).collect()
-lines = lines.rdd.map(lambda x : (x.split('\t')[0],x.split('\t')[1]))
 
+# fonction utile
 def f(x): 
 	return x
 
-
+# gérer la fusion des values dans le reduce by key
 def get_res(x,y):
 	if x=="B" or y =="B" :
 		return False
 	else:
 		return True
 
+#le reduceByKey ne transforme pas les élément unique d'une key, on les gere donc grace a cette fonction
 def replacee(var):
 	if var == "B":
 		return False
@@ -20,9 +18,14 @@ def replacee(var):
 		return True
 	else :
 		return var
+	
+	
+	
+lines = sc.textFile("/Users/raphaeluzan/Desktop/mapreduce/data.txt") # load les données en rdd
+lines = lines.map(lambda x : (x.split('\t')[0],x.split('\t')[1].split())) #on split le type avec les lignes elles memes
+lines = lines.flatMapValues(f).map(lambda x : (x[1],x[0])).reduceByKey(lambda x,y : get_res(x,y) ) # on flatmap les values et 
+#on inverse les Key avec les values pour pouvoir fusionner par mot en fonction du texte auxquel il appartient
 
-lines = lines.flatMapValues(f).map(lambda x : (x[1],x[0])).reduceByKey(lambda x,y : get_res(x,y) )
-lines.collect()
+lines.map(lambda x : (x[0],replacee(x[1]))).collect()#on gere les clé avec une unique value que le reduceByKey a ignore
 
 
-lines.map(lambda x : (x[0],replacee(x[1]))).collect()
